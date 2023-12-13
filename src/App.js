80f84b2310e6dc1f2790367d4657bdbff4bd8fd9
MuskanFatimaa <hellodearmuskan@gmail.com>
2023-12-13 18:49:03 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Header />
+   <HeroSection />
+   <Footer />
+   </>
+  );
+}
+
+export default App;
